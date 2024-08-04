6d9494f69f2fde3d06a804afe2c3bc35dd3e3631
Aref <arefshal@yahoo.com>
2024-08-04 10:14:53 +0330

    Initial Commit

---
+++ Library ManagerTests/Library_ManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Library_ManagerTests.swift
+//  Library ManagerTests
+//
+//  Created by Aref on 8/4/24.
+//
+
+import Testing
+@testable import Library_Manager
+
+struct Library_ManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
