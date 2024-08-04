2ec83019af643a30b6ee8b334fa6f8157c687217
Aref <arefshal@yahoo.com>
2024-08-04 12:15:31 +0330

    feat(book): add Book struct
    Added a new struct representing a book with title, author, and category fields.

---
+++ Library Manager/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  Library Manager
+//
+//  Created by Aref on 8/4/24.
+//
+
+import Foundation
+/// A structure representing a book
+struct Book {
+    let title: String
+
+    let author: String
+
+    let category: String
+}
