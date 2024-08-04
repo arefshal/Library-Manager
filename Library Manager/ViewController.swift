0f4ff0e8571499fc56c55635f903caa9807382a2
Aref <arefshal@yahoo.com>
2024-08-04 12:20:49 +0330

    feat(view): add properties and UI elements to ViewController

--- Library Manager/ViewController.swift
+++ Library Manager/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Properties
 
+    let categories = ["Scientific", "Novel", "Historical", "Others"]
+    var selectedCategory: String = ""
+    var books: [Book] = []
+    var filteredBooks: [Book] = []
+    var selectedFilterCategory: String = "All"
+
+    // MARK: - UI Elements
+
+    var tableView: UITableView!
+    var titleTextField: UITextField!
+    var authorTextField: UITextField!
+    var categoryButton: UIButton!
+    var addButton: UIButton!
+    var pickerView: UIPickerView!
+    var filterButton: UIButton!
+    var filterPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
